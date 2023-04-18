9fcc659af38b73e999184e009f13c83e558e130d
sushmitha1125 <130980595+sushmitha1125@users.noreply.github.com>
2023-04-18 19:04:48 +0530

    springboot

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=5;
+		Test t=new Test();
+		t.add(a, b);
+		t.mul(a, b);
+		t.sub(a, b);
+		t.div(a, b);
+		t.square(a);
+		t.cube(a);
+	}
+}
---
+++ src/Test.java
@@ -0,0 +1,36 @@
+
+public class Test {
+
+	public void add(int a,int b) {
+		int result=a+b;
+    
+	System.out.println("the addition of two numbers is"+result);
+}
+	public void mul(int a,int b) {
+		int result=a*b;
+		
+		System.out.println("the multiplication of two numbers is"+result);
+	}
+	
+	public void sub(int a,int b) {
+		int result=a-b;
+		
+		System.out.println("the substraction of two numbers is"+result);
+	}
+	public void div(int a,int b) {
+		int result=a/b;
+		
+		System.out.println("the division of two numbers is"+result);
+	}
+	
+	public void square(int a) {
+		int result=a*a;
+		
+		System.out.println("the square of a is"+result);
+	}
+	public void cube(int a) {
+		int result=a*a*a;
+		
+		System.out.println("the cube of a is"+result);
+	}
+}
